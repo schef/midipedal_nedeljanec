eb4724b87665218dba2c91bb0efb68f408a37b2a
Stjepan Horvat <zvanstefan@gmail.com>
2016-02-18 13:11:07 +0100

    reseno

---
+++ arduino/connections_test/connections_test.ino
@@ -0,0 +1,82 @@
+const int selectPin[] = { 10, 11, 12, 9 };
+const int dataPin[] = { 8, 7, 6, 5, 4, 3, 2 };
+
+int keyNote[4][7];
+int keyNoteOld[4][7];
+
+long lastDebounceTime[4][7];
+long debounceDelay = 5;
+
+void initPins(){
+
+    for (int i = 0; i < 4; i++){
+      pinMode(selectPin[i], OUTPUT);
+    }
+    for (int i = 0; i < 7; i++){
+      pinMode(dataPin[i], INPUT);
+    }
+    
+    //for (int s = 0; s < 6; s++) {
+    //  for (int d = 0; d < 12; d++) {
+    //    lastDebounceTime[s][d] = 0;
+    //  }
+    //}
+    
+}
+
+void readNote(){
+    //select
+    for (int s = 0; s < 4; s++) {
+        digitalWrite(selectPin[s], HIGH);
+
+        //data
+        for (int d = 0; d < 7; d++) {
+            keyNote[s][d] = digitalRead(dataPin[d]);
+            //if (keyNote[s][d] != keyNoteOld[s][d]){
+            //  lastDebounceTime[s][d] = millis();
+            //}
+        }
+        digitalWrite(selectPin[s], LOW);
+    }
+}
+
+void setup()   {                
+  //Serial.begin(38400);
+  Serial.begin(9600);
+  initPins();
+}
+
+void loop() {
+  readNote();
+  
+  for (int s = 0; s < 4; s++){
+    for (int d = 0; d < 7; d++){
+     // if ((millis() - lastDebounceTime[s][d]) > debounceDelay) {
+      if (keyNote[s][d] == 1 && keyNote[s][d] != keyNoteOld[s][d]){
+        
+        if ((millis() - lastDebounceTime[s][d]) > debounceDelay) {
+        Serial.print(s);
+        Serial.print(":");
+        Serial.print(d);
+        Serial.println("=1");
+        //usbMIDI.sendNoteOn(35+s*5+d, 100, s+1);
+        //delay(100);
+        keyNoteOld[s][d] = keyNote[s][d];
+        lastDebounceTime[s][d] = millis();
+        }
+      }
+        else if (keyNote[s][d] == 0 && keyNote[s][d] != keyNoteOld[s][d]){
+          if ((millis() - lastDebounceTime[s][d]) > debounceDelay) {
+        Serial.print(s);
+        Serial.print(":");
+        Serial.print(d);
+        Serial.println("=0");
+        //usbMIDI.sendNoteOff(35+s*5+d, 100, s+1);
+        //delay(100);
+        keyNoteOld[s][d] = keyNote[s][d];
+        lastDebounceTime[s][d] = millis();
+        }
+      }
+    }
+  }
+}
---
+++ pcb/midipedal_rupa.Part1 [Profile1].nc
@@ -0,0 +1,35 @@
+( Made using CamBam - http://www.cambam.co.uk )
+( midipedal_rupa 2/17/2016 10:06:55 AM )
+( T0 : 3.0 )
+G21 G90 G64 G40
+G0 Z3.0
+( T0 : 3.0 )
+T0 M6
+( Profile1 )
+G17
+M3 S1000
+G0 X2.2 Y-0.05
+G0 Z1.0
+G1 F300.0 Z-3.2
+G1 F800.0 X-7.5
+G1 Y0.05
+G1 X2.2
+G1 Y-0.05
+G1 F300.0 Z-6.4
+G1 F800.0 X-7.5
+G1 Y0.05
+G1 X2.2
+G1 Y-0.05
+G1 F300.0 Z-9.6
+G1 F800.0 X-7.5
+G1 Y0.05
+G1 X2.2
+G1 Y-0.05
+G1 F300.0 Z-12.7
+G1 F800.0 X-7.5
+G1 Y0.05
+G1 X2.2
+G1 Y-0.05
+G0 Z3.0
+M5
+M30
